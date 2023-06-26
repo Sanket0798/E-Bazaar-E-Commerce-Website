fc141a95c395da075f42c6d41c39942ab5b082ee
Sanket0798 <rathodsanket2022@gmail.com>
2023-06-26 12:52:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Product from './pages/Product'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
+import Home from './pages/Home'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from 'react-router-dom';
+
+
+function App() {
+
+  return (
+<Home/>
+  )
+}
+
+export default App
